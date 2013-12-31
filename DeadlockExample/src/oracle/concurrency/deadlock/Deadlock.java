54c747cf62b854414544375814721d2b3f775c46
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-12-31 15:10:51 +0800

    Deadlock describes a situation where two or more threads are blocked forever, waiting for each other. Here's an example.

---
+++ DeadlockExample/src/oracle/concurrency/deadlock/Deadlock.java
@@ -0,0 +1,77 @@
+package oracle.concurrency.deadlock;
+
+import static java.lang.System.out;
+
+/**
+ * @author WWC076
+ * <p>
+ * 	<a href="http://docs.oracle.com/javase/tutorial/essential/concurrency/deadlock.html">Deadlock</a>
+ * </p>
+ */
+public class Deadlock {
+	/**
+	 * @author WWC076
+	 * Inner class Friend
+	 */
+	static class Friend {
+		private final String name;
+		
+		/**
+		 * Constructor
+		 * @param name
+		 */
+		public Friend(String name){
+			this.name = name;
+		}
+		
+		/**
+		 * Get friend name
+		 * @return name
+		 */
+		public String getName(){
+			return this.name;
+		}
+		
+		/**
+		 * Bow to friend
+		 * @param bower
+		 */
+		public synchronized void bow(Friend bower){
+			out.format("%s: %s" + " has bowed to me!%n", this.name, bower.getName());
+			bower.bowBack(this);
+		}
+		
+		/**
+		 * Bow back to friend
+		 * @param bower
+		 */
+		public synchronized void bowBack(Friend bower){
+			out.format("%s: %s" + " has bowed backed to me!%n", this.name, bower.getName());
+		}
+	}
+	
+	/**
+	 * Main program
+	 * @param args
+	 */
+	public static void main(String[] args){
+		final Friend alphonse = new Friend("Alphonse");
+		final Friend gaston = new Friend("Gaston");
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				alphonse.bow(gaston);
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				gaston.bow(alphonse);
+			}
+		}).start();
+	}
+}
